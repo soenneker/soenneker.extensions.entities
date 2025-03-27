e79f96fb50892287b5ab8fa0f5fcdcbe1cd74957
Jake Soenneker <jake@soenneker.com>
2025-03-27 09:46:15 -0500

    Initial commit

---
+++ src/EntitiesExtension.cs
@@ -0,0 +1,30 @@
+﻿using Soenneker.Entities.Entity.Abstract;
+using Soenneker.Extensions.String;
+
+namespace Soenneker.Extensions.Entities;
+
+/// <summary>
+/// A collection of helpful Entities extension methods
+/// </summary>
+public static class EntitiesExtension
+{
+    /// <summary>
+    /// Shorthand for SplitId on the Id of the entity.
+    /// </summary>
+    /// <returns>
+    /// Will not return the partitionKey unless it's the same as the documentId
+    /// </returns>
+    public static string ToDocumentId<T>(this T entity) where T : IEntity
+    {
+        (_, string documentId) = entity.Id.ToSplitId();
+
+        return documentId;
+    }
+
+    public static string ToPartitionKey<T>(this T entity) where T : IEntity
+    {
+        (string partitionKey, _) = entity.Id.ToSplitId();
+
+        return partitionKey;
+    }
+}
---
+++ test/Soenneker.Extensions.Entities.Tests/EntitiesExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Entities.Tests;
+
+public sealed class EntitiesExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
