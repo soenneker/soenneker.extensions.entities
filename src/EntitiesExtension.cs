b8a295a6910abe9ec5523acb5bdeb3af66908167
Jake Soenneker <jake@soenneker.com>
2026-01-08 17:15:41 -0600

    Attributes

--- src/EntitiesExtension.cs
+++ src/EntitiesExtension.cs
@@ -1,5 +1,7 @@
 ﻿using Soenneker.Entities.Entity.Abstract;
 using Soenneker.Extensions.String;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace Soenneker.Extensions.Entities;
 
@@ -14,6 +16,8 @@ public static class EntitiesExtension
     /// <returns>
     /// Will not return the partitionKey unless it's the same as the documentId
     /// </returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToDocumentId<T>(this T entity) where T : IEntity
     {
         (_, string documentId) = entity.Id.ToSplitId();
@@ -21,6 +25,8 @@ public static class EntitiesExtension
         return documentId;
     }
 
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToPartitionKey<T>(this T entity) where T : IEntity
     {
         (string partitionKey, _) = entity.Id.ToSplitId();
